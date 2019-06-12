cc5a2723c7d491523eb82f6ee5dc53df5df9ffd4
Aluno <you@example.com>
2019-06-12 20:52:30 -0300

    implementar a home de forma estática

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  perfis = [];
 
+  constructor() {
+    this.perfis = [
+      {
+        'avatar':'http://img.ibxk.com.br/ns/rexposta/2018/12/28/28144435736741.jpg?watermark=neaki&w=600',
+        'nome':'Brunin',
+        'idade':18,
+        'likes':0
+      },
+      {
+        'avatar':'https://i.ytimg.com/vi/r2t8inao-iE/maxresdefault.jpg',
+        'nome':'10/10',
+        'idade':15,
+        'likes':0
+      }
+    ]
+  }
 }
